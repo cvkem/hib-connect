2fe30db22d7dbbc6218c42a87584f6281bfc4dbf
Cees van Kemenade <pentaho@vinzi.nl>
2012-01-31 18:33:52 +0100

    Added code to initialized from a config file. Code does not work yet. The programmatic configuration-interface of hibernate does not seem to work (??).

--- src/vinzi/hib_connect/bridge.clj
+++ src/vinzi/hib_connect/bridge.clj
@@ -1,5 +1,6 @@
 (ns vinzi.hib-connect.bridge
   (use vinzi.hib-connect.globals)
+  (require [clojure.string :as str])
   (import org.hibernate.SessionFactory)
   (import org.hibernate.cfg.Configuration))
 
@@ -9,51 +10,175 @@
 ;;  Functions for managing the connections
 ;;
 
-(defn createSessionFactory "Create a sessionFactory" []
+;; if dbParam is nill the hibernate will be initialized from the
+;; hibernate.settings.xml. If db is set this will be used to initialize.
+;;switched back to 1.2
+;;(def ^:dynamic dbParams nil)
+(def dbParams nil)
+
+;; a global variable is used to store the sessionFactory.
+;; This sessionFActory is used by the call-with-hibernate-session.
+;;(def ^:dynamic sessionFactory nil)
+(def  sessionFactory nil)
+
+
+
+(def csfPropMap {:classname "connection.driver_class"
+		 :url "connection.url"
+		 :user "connection.username"
+		 :dialect "dialect"
+		 :password "connection.password"
+		 :pool-size "connection.pool_size"
+		 :current_session_context_class "current_session_context_class"
+		 :cache-provider-class "cache.provider_class"
+		 :show-sql  "show_sql"
+		 :hbm2ddl-auto "hbm2dll.auto"})
+
+;; default values for all properties
+(def csfPropDef {:classname "org.hsqldb.jdbcDriver"
+		 :url "jdbc:hsqldb:hsql://localhost/testdb"
+		 :dialect "org.hibernate.dialect.HSQLDialect"
+		 :user "user"
+		 :password ""
+		 :pool-size "1"
+		 :current_session_context_class "thread"
+		 :cache-provider-class "org.hibernate.cache.NoCacheProvider"
+		 :show-sql  "true"
+		 :hbm2ddl-auto "update"})
+
+
+;; mapping from subprotocol to dialect
+;; Note: For mySql you also have other options
+;; - MySQL5InnoDBDialect 	 
+;; - MySQLDialect 	An SQL dialect for MySQL (prior to 5.x).
+;; - MySQLInnoDBDialect 	 
+;; - MySQLMyISAMDialect
+;; For  Postgress you also have option 'PostgresPlusDialect'
+;; For hypersonic you also have option
+;;     'HSQLDialect.ReadUncommittedLockingStrategy'
+(def csfDialectMap {"mysql"          "MySQL5Dialect" 
+		    "hsqldb:hsql"    "HSQLDialect"
+		    "postgresql"     "PostgreSQLDialect"})
+
+(defn set-db-params "Set the dbParam record based on the baseProperties that are passed"
+  [baseProps]
+  (letfn [(check-dialect [db]
+			 ;; try to derive the dialect from the subprotocol
+			 ;; if it is not set
+			 (if (:dialect db)
+			   db
+			   (let [subprotocol (:subprotocol db)
+				 dialect (get csfDialectMap subprotocol)
+				 dialect (when dialect (str
+					     "org.hibernate.dialect." dialect))]
+			     (if dialect (assoc db :dialect dialect) db))))
+	  (derive-url [db]
+		      ;; derive the url from db-host, subprotocol,
+		      ;; db-port and db-name and replace by the url
+		      ;; (example jdbc:hsqldb:hsql://localhost:9001/testdb)
+		      (let [{:keys [db-host subprotocol db-port db-name]} db
+			    db (dissoc db :db-host :subprotocol
+				       :db-port :db-name)
+			    db-port (when db-port (str ":" db-port))
+			    url (str "jdbc:" subprotocol
+				     "://" db-host db-port
+				     "/" db-name)]
+			(assoc db :url url)))
+	  (add-defaults [db]
+			(into csfPropDef db))]
+    (let [;; extract the list of resources (white-space separated)
+	  resources (:resources baseProps)
+	  resources (when resources(-> resources
+				       (str/trim)
+				       (str/split #"\s+")))
+	  ;; pre-process the db-params record
+	  db (-> baseProps
+		 (dissoc :resources) 
+		 (check-dialect)
+		 (derive-url)
+		 (add-defaults))]
+      ;; set the (dynamic) var dbParam with the obtained result
+      (def dbParams {:db        db
+		     :resources resources}))))
+    
+(defn props-createSessionFactory "Create a sessionFactory on the properties mentioned in the dbParam record." [{:keys [db resources]}]
+  (letfn [(set-property [csf kv]
+			;; set property given by the key-value pair to 'csf'
+			(let [k (key kv)
+			      v (val kv)
+			      p (get csfPropMap k)]
+			  (println " for key '" k "' set property '" p
+				   "' to value: " v)
+			  (if p
+			    (let [ret (.setProperty csf p v)]
+			      (println "  returning: " ret)(flush)
+			      ret)
+			    (do
+			      (println "WARNING: Property: " k
+				       "with value: " v
+				       "is not recognized")
+			      (println "   ... option ignored")(flush)
+			      csf))))
+	  (add-resource [csf res]
+			;; add a resource to the config 'csf'
+			(let [ret (.addResource csf res)]
+			  (println "resource " res " returned: "ret) (flush)
+			  ret))]
+    (println "Setting up a session factory with")
+    (println "   Params: " db)
+    (println "   Resources: " resources) (flush)
+    (try
+      ;; build a configuration
+      (let [csf (Configuration.)
+	    ;; process dialect before connection properties
+	    dialect (:dialect db)
+	    db   (dissoc db :dialect)
+	    csf (if dialect (.setProperty csf "dialect" dialect) csf)
+	    csf (reduce set-property csf db)
+	    csf (reduce add-resource csf resources)]
+	;; and build and return the SessionFactory
+	(println "csf = " csf)
+	(.buildSessionFactory csf))
+      (catch Throwable except
+	(println "Exception: " (.getMessage except))))))
+;;     (.. (Configuration.)
+;; 	;;    <!-- SQL dialect -->
+;; 	(setProperty "dialect" 	"org.hibernate.dialect.HSQLDialect")
+;; 	;;
+;; 	(setProperty "connection.driver_class" 	)
+;; 	(setProperty "connection.url" 	)
+;; 	(setProperty "connection.username" 	"sa")
+;; 	(setProperty "connection.password" 	"")
+;; 	;;    <!-- JDBC connection pool (use the built-in) -->
+;; 	(setProperty "connection.pool_size" 	"1")
+;; 	;;    <!-- Enable Hibernate's automatic session context management -->
+;; 	(setProperty "current_session_context_class" 	"thread")
+;; 	;;    <!-- Disable the second-level cache -->
+;; 	(setProperty "cache.provider_class" 	)
+;; 	;;    <!-- Echo all executed SQL to stdout -->
+;; 	(setProperty "show_sql" 	)
+;; 	;;   <!-- Drop and re-create the database schema on startup -->
+;; 	(setProperty "hbm2ddl.auto" 	"update")
+;; ;;
+;; 	(buildSessionFactory))
+;;     (catch Throwable except
+;;       (println "Exception: " (.getMessage except)))))
+
+(defn xml-createSessionFactory "Create a sessionFactory based on the hibernate.settings.xml files at the class-path." []
+  (println "Setting up a hibernate session-factory from xml")
   (try
     (.. (Configuration.) (configure) (buildSessionFactory))
     (catch Throwable except
       (println "Exception: " (.getMessage except)))))
 
+(defn createSessionFactory "Create a sessionFactory. Use 'dbParams' if available, otherwise look for an xml file." []
+  (if dbParams
+    (props-createSessionFactory dbParams)
+    (xml-createSessionFactory))) 
 
-(comment
-(defn createSessionFactory "Create a sessionFactory" []
-  (try
-;;    (.. (Configuration.) (configure) (buildSessionFactory))
-;;    (.. (Configuration.) (configure "hibernate.cfg.xml") (buildSessionFactory))
-;;    (.. (Configuration.)  (buildSessionFactory))
-(comment
-    (.. (Configuration.)
-;;    <!-- SQL dialect -->
-(setProperty "dialect" 	"org.hibernate.dialect.HSQLDialect")
-;;
-(setProperty "connection.driver_class" 	"org.hsqldb.jdbcDriver")
-(setProperty "connection.url" 	"jdbc:hsqldb:hsql://localhost/testdb")
-(setProperty "connection.username" 	"sa")
-(setProperty "connection.password" 	"")
-;;    <!-- JDBC connection pool (use the built-in) -->
-(setProperty "connection.pool_size" 	"1")
-;;    <!-- Enable Hibernate's automatic session context management -->
-(setProperty "current_session_context_class" 	"thread")
-;;    <!-- Disable the second-level cache -->
-(setProperty "cache.provider_class" 	"org.hibernate.cache.NoCacheProvider")
-;;    <!-- Echo all executed SQL to stdout -->
-(setProperty "show_sql" 	"true")
- ;;   <!-- Drop and re-create the database schema on startup -->
-(setProperty "hbm1ddl.auto" 	"update")
-;;
-	(buildSessionFactory))
-)  ;;; end comment
-    (.. (Configuration.)
-	(addResource "mapping/hibernateMapping.xml")
-	 (buildSessionFactory))
-    (catch Throwable except
-      (println "Exception: " (.getMessage except)))))
-)
 
-;; a global variable is used to store the sessionFactory.
-;; This sessionFActory is used by the call-with-hibernate-session.
-(def sessionFactory (createSessionFactory))
+
+;;(def ^:dynamic sessionFactory nil)
 
 
 (defn close-hib "close the session-factory (shutdown)" []
@@ -62,13 +187,14 @@
 (defn force-open-hib
   "Check the status of the current session. If it is closed reopen it."
   []
-  ;;(println "Force-open-hib:")
-  ;;(println "Currently having sessionFactory" sessionFactory)
+  (println "Force-open-hib:")
+  (println "Currently using sessionFactory" sessionFactory)
   ;;(when sessionFactory
     ;;(println "  status " (.isClosed sessionFactory)))
   (when (or (nil? sessionFactory)
 	    (.isClosed sessionFactory))
-    (def sessionFactory (createSessionFactory))))
+    (def sessionFactory (createSessionFactory))
+    (println "generated sessionFactory: " sessionFactory)))
 
 
 (defn exec-hib-transaction [f & args]
